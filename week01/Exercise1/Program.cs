687726a507eaaac019e343ddb299beb56bf35fa9
Destiny Linus <youngdestinx@gmail.com>
2025-05-07 12:20:25 +0100

    done exercise one and updated the READ.md

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        string formattedFirstName = firstName.Substring(0, 1).ToUpper() + firstName.Substring(1).ToLower();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        string formattedLastName = lastName.Substring(0, 1).ToUpper() + lastName.Substring(1).ToLower();
+
+        Console.WriteLine($"Your name is {formattedLastName}, {formattedFirstName} {formattedLastName}.");
     }
 }
