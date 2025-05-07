e5411214299ace2b598f3aa425a79929abd6cc58
Destiny Linus <youngdestinx@gmail.com>
2025-05-07 17:08:28 +0100

    loop exercise done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int number = random.Next(1, 101);
+
+        while (true)
+        {
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            int guessNumber = int.Parse(guess);
+
+            if (guessNumber > number)
+            {
+                Console.WriteLine("The guess is higher");
+            }
+
+            else if (guessNumber < number)
+            {
+                Console.WriteLine("The guess is lower");
+            }
+
+            else
+            {
+                Console.WriteLine("The guess is correct");
+                break;
+            }
+        }
     }
 }
