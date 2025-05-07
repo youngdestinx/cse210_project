ce29b83b7eb7c3dff80613efc22311f57ce3686f
Destiny Linus <youngdestinx@gmail.com>
2025-05-07 13:34:21 +0100

    exercise 2 done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? (eg: 70) ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+          letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+          letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+          letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+          letter = "D";
+        }
+
+        else 
+        {
+          letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the course!");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, but you failed the course, please try again next time");
+        }
+
     }
+    
 }
